1e6cc69a91df0d485b2017b1076aa6bb865965f5
dinobaranasic <dino.baranasic8@gmail.com>
2019-12-11 12:29:07 +0100

    Prvi dio izgleda gotov. Potrebno jos napraviti polja za unos ili odabir te formu za ispis podataka.

--- CAN BUS komunikacija/Form1.Designer.cs
+++ CAN BUS komunikacija/Form1.Designer.cs
@@ -28,25 +28,211 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.buttonCopyTelegram = new System.Windows.Forms.Button();
+            this.buttonCopySignal = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ink Free", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Ink Free", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Output";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Ink Free", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(170, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 30);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CAN ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Ink Free", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(170, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 30);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CAN ID";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Ink Free", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(452, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 30);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Payload";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Ink Free", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(398, 135);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(149, 30);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Def. Payload";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Ink Free", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(893, 65);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 30);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Signal";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Ink Free", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(893, 135);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(79, 30);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Signal";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(976, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(370, 23);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Signal- Redni broj bytea unutar payloada";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Ink Free", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(12, 276);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(363, 34);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "Ciklus slanja poruka(ms): -->";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Ink Free", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(12, 226);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(325, 34);
+            this.label11.TabIndex = 9;
+            this.label11.Text = "Broj poslanih poruka : -->";
+            // 
+            // buttonCopyTelegram
+            // 
+            this.buttonCopyTelegram.BackColor = System.Drawing.Color.Black;
+            this.buttonCopyTelegram.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonCopyTelegram.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCopyTelegram.ForeColor = System.Drawing.Color.White;
+            this.buttonCopyTelegram.Location = new System.Drawing.Point(63, 405);
+            this.buttonCopyTelegram.Margin = new System.Windows.Forms.Padding(5);
+            this.buttonCopyTelegram.Name = "buttonCopyTelegram";
+            this.buttonCopyTelegram.Size = new System.Drawing.Size(150, 75);
+            this.buttonCopyTelegram.TabIndex = 11;
+            this.buttonCopyTelegram.Text = "   Send\r\nCopyTelegram";
+            this.buttonCopyTelegram.UseVisualStyleBackColor = false;
+            // 
+            // buttonCopySignal
+            // 
+            this.buttonCopySignal.BackColor = System.Drawing.Color.Black;
+            this.buttonCopySignal.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonCopySignal.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCopySignal.ForeColor = System.Drawing.Color.White;
+            this.buttonCopySignal.Location = new System.Drawing.Point(1150, 385);
+            this.buttonCopySignal.Margin = new System.Windows.Forms.Padding(5);
+            this.buttonCopySignal.Name = "buttonCopySignal";
+            this.buttonCopySignal.Size = new System.Drawing.Size(150, 75);
+            this.buttonCopySignal.TabIndex = 12;
+            this.buttonCopySignal.Text = "  Send\r\nCopySignal";
+            this.buttonCopySignal.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.DarkCyan;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::CAN_BUS_komunikacija.Properties.Resources.backimage;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1177, 741);
+            this.ClientSize = new System.Drawing.Size(1358, 828);
+            this.Controls.Add(this.buttonCopySignal);
+            this.Controls.Add(this.buttonCopyTelegram);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Ink Free", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.Window;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1374, 867);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1374, 867);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CAN BUS Komunikacija";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button buttonCopyTelegram;
+        private System.Windows.Forms.Button buttonCopySignal;
     }
 }
 
